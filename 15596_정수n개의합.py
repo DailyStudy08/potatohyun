c6afdf04e0384189fe90cea824e82a1aa0887999
unknown <91wlgys@naver.com>
2022-08-04 22:17:33 +0900

    드디어 나도 커밋

---
+++ 15596_정수n개의합.py
@@ -0,0 +1,17 @@
+# def solve(a):  
+#     # num_list = list(map(int,a.split(',')))
+#     ans = 0
+#     for i in a:
+#         ans += i
+#     return ans
+# numm = eval(input())
+# print(solve(numm))
+
+def solve(a):  
+    # num_list = list(map(int,a.split(',')))
+    ans = 0
+    for i in a:
+        ans += i
+    return ans
+print(solve(eval(input())))
+
